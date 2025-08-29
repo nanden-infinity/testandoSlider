2383cc7e5752faf11dfbb914fca52d15df1ba3cd
Inanden <147476553+Inanden@users.noreply.github.com>
2025-08-29 13:55:31 +0100

    pokemons append

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
-const buttonMenu = document.getElementById("menu--btn");
+// pegar(url);
 
-const eventTarget = ["click", "dbclick"];
-eventTarget.forEach((event) => buttonMenu.addEventListener(event, openMenu));
+// const buttonMenu = document.getElementById("menu--btn");
 
-function openMenu(ev) {
-  console.log(ev);
-  const menuOpen = this.querySelector(".menu");
-  console.log(menuOpen);
-  document.body.classList.toggle("class--menu")
+// const eventTarget = "click";
+// buttonMenu.addEventListener(eventTarget, openMenu)
+
+// function openMenu(ev) {
+//   console.log(ev);
+//   const menuOpen = this.querySelector(".menu");
+//   console.log(menuOpen);
+//   document.body.classList.toggle("class--menu");
+// }
+
+//  function pegar(href) {
+//   const dados =  href
+
+//   console.log("results", dados);
+// }
+
+// console.log("ola mundo");
+
+const url = "https://pokeapi.co/api/v2/pokemon?limit=10";
+
+onload = function () {
+  pegarApi(url)
+};
+const container =  document.querySelector(".wrapper")
+async function pegarApi(dados) {
+  const dadosApi = await (await fetch(dados)).json();
+  const {results} = dadosApi
+results.map(async({name,url}) => {
+  const resultApi = await (await fetch(url)).json()
+  const {
+    sprites: { front_default:images },
+  } = resultApi;
+  const html = `
+   <figure class"border">
+              
+              <div class="circlo--img"><img src="${images}" alt=""></div>
+              <h1>${name}</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nesciunt aliquid delectus!</p>
+              <button class="btn rounded">Find More</button>
+            </figure>`;
+
+            container.insertAdjacentHTML("afterbegin", html)
+})
+
+
+ 
 }
